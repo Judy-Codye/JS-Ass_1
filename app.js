59f5b30f41c108020b2c00190f39e39bbaeb3877
Nasira Siddiqui <ns670825@gmail.com>
2025-06-10 03:17:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+// // Chapter-1 Alert
+
+// Q-1
+alert("Hello World");
+
+// Q-2
+alert("Error!Please enter a valid password");
+
+// Q-3
+alert("Welcome to js land....Happy Coding!");
+
+// Q-4
+alert("Welcome to JS land...");
+alert("Happy Coding!");
+
+// Q-5
+console.log("Hello...I can run JS through my web brower's console");
+
+// // Chapter-2 variable
+
+// Q-1
+var name = "Nasira";
+alert(name);
+
+// Q-2
+var myFullname = "Nasira Siddiqui";
+alert(myFullname);
+
+// Q-3
+var message = "Hello World";
+alert(message);
+
+// Q-4
+var name = "Jhone Doe";
+alert(name);
+
+var age = "15 years old";
+alert(age);
+
+var course = "Certified Mobile Application Development";
+alert(course);
+
+// Q-5
+var name = "PIZZA"
+alert(name);
+name = "PIZZ";
+alert(name);
+name = "PIZ";
+alert(name);
+name = "PI";
+alert(name);
+name = "P";
+alert(name);
+
+// Q-6
+var email = ("My email address is example@example.com");
+alert(email);
+
+// Q-7
+var book = ("I am trying to learn from the Book A smarter way to learn JavaScript");
+alert(book);
+
+// Q-8
+var message = ("Yah! I can write HTML content through JavaScript");
+console.log(message);
+
+// Q-9
+var name = ("▬▬▬▬▬▬▬▬▬௹⨝ῼ⨝௹▬▬▬▬▬▬▬▬▬")
+alert(name);
+console.log(name);
+
+
